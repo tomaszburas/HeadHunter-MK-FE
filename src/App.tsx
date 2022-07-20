0a827e1cf649b369ae8521e95d8bf1a6724a48f4
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-07-20 15:13:35 +0200

    Added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-export const App = () => {
-  return (
-    <div className="App">
+import React from 'react';
+import styled from 'styled-components';
 
-    </div>
-  );
-}
+export const App = () => {
+  return <Container>Head Hunter</Container>;
+};
+
+const Container = styled.div`
+  font-size: ${(props) => props.theme.fontSize.base};
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import './index.css';
+import {App} from './App';
+import {Theme} from './components/Theme';
+import GlobalCSS from './global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <GlobalCSS />
+      <App />
+    </Theme>
   </React.StrictMode>
 );
