f974b2833c4d416f1f9ea62259d826ea371322b3
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-07-21 12:40:50 +0200

    Added AccountActivation view

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import {Route, Routes} from 'react-router-dom';
 import {Login} from './views/Login';
+import {AccountActivation} from './views/AccountActivation';
 
 export const App = () => {
   return (
     <Container>
       <Routes>
         <Route path={`/login`} element={<Login />} />
+        <Route path={`/activation`} element={<AccountActivation />} />
       </Routes>
     </Container>
   );
