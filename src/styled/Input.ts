9c3659ebca6291da71f0e0c50d6b0f7b7961b92d
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-07-22 10:56:21 +0200

    Deleted margin-bottom

--- src/styled/Input.ts
+++ src/styled/Input.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Input = styled.input`
   width: 100%;
   padding: ${(props) => props.theme.paddingSize.sm};
-  margin-bottom: ${(props) => props.theme.marginSize.sm};
   border: none;
   background-color: ${(props) => props.theme.colors.black};
   color: ${(props) => props.theme.colors.white};
