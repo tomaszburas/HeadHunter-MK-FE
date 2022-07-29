a9adbc43a455c011b5ec2d5ea5f6dec852b1b7b9
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-07-29 11:44:11 +0200

    Added Available Students views

---
+++ src/components/Hr/AvailableStudents/AvailableStudents.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ContainerHrStudents} from '../../../assets/styled/Hr/ContainerHrStudents';
+import {AvailableStudent} from './AvailableStudent';
+
+export const AvailableStudents = () => {
+  return (
+    <ContainerHrStudents>
+      <AvailableStudent />
+      <AvailableStudent />
+      <AvailableStudent />
+      <AvailableStudent />
+      <AvailableStudent />
+    </ContainerHrStudents>
+  );
+};
