7ddc04a6fd7c10feff1f1a410c499fa9a6725a1a
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-07-30 13:55:15 +0200

    Added view

---
+++ src/components/Admin/AddHrForm.tsx
@@ -0,0 +1,39 @@
+import {FormAccount} from '../../assets/styled/Form/FormAccount';
+import {InputWrapper} from '../../assets/styled/Form/InputWrapper';
+import {Input} from '../../assets/styled/Input';
+import {Button} from '../Button';
+import React from 'react';
+
+export const AddHrForm = () => {
+  return (
+    <FormAccount>
+      <InputWrapper>
+        <div className="label-box">
+          <label htmlFor="fullname">Imię i nazwisko:</label>
+        </div>
+        <Input type="text" id="fullname" />
+      </InputWrapper>
+      <InputWrapper>
+        <div className="label-box">
+          <label htmlFor="email">Email:</label>
+        </div>
+        <Input type="text" id="email" />
+      </InputWrapper>
+      <InputWrapper>
+        <div className="label-box">
+          <label htmlFor="company">Firma:</label>
+        </div>
+        <Input type="text" id="company" />
+      </InputWrapper>
+      <InputWrapper>
+        <div className="label-box">
+          <label htmlFor="maxStudents">Liczba osób do rezerwacji:</label>
+        </div>
+        <Input type="number" id="maxStudents" />
+      </InputWrapper>
+      <div className="btn-box">
+        <Button text="Zapisz" type="submit" />
+      </div>
+    </FormAccount>
+  );
+};
---
+++ src/views/Admin/AdminMain.tsx
@@ -0,0 +1,56 @@
+import {Header} from '../../components/Header';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Button} from '../../components/Button';
+import styled from 'styled-components';
+
+export const AdminMain = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="box">
+          <h1>Cześć Jan Kowalski 👋</h1>
+          <Link to="/admin/edit" className="btn-box">
+            <Button text="Edytuj konto" />
+          </Link>
+          <Link to="/admin/add-students" className="btn-box">
+            <Button text="Dodaj kursantów" />
+          </Link>
+          <Link to="/admin/add-hr" className="btn-box">
+            <Button text="Dodaj hr" />
+          </Link>
+          <Link to="/admin/logout" className="btn-box">
+            <Button text="Wyloguj" />
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  padding: ${(props) => props.theme.paddingSize.lg} 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  .box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+
+    h1 {
+      margin-bottom: ${(props) => props.theme.marginSize.base};
+    }
+
+    .btn-box:not(:last-child) {
+      margin-bottom: ${(props) => props.theme.marginSize.sm};
+    }
+
+    .btn-box {
+      width: 100%;
+    }
+  }
+`;
