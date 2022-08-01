3a5581a124ffd078a1cf4b62556f73be77eebf02
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-08-01 11:51:49 +0200

    Added feature react-papaparse

--- src/components/Admin/AddStudentsForm.tsx
+++ src/components/Admin/AddStudentsForm.tsx
@@ -1,16 +1,41 @@
 import {FormAccount} from '../../assets/styled/Form/FormAccount';
 import {InputWrapper} from '../../assets/styled/Form/InputWrapper';
-import {Input} from '../../assets/styled/Input';
 import {Button} from '../Button';
+import {useCSVReader} from 'react-papaparse';
+import styled from 'styled-components';
+import {FormEvent, useState} from 'react';
 
 export const AddStudentsForm = () => {
+  const {CSVReader} = useCSVReader();
+  const [file, setFile] = useState();
+
+  const handleForm = (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(file);
+  };
+
   return (
-    <FormAccount>
+    <FormAccount onSubmit={handleForm}>
       <InputWrapper>
         <div className="label-box">
           <label htmlFor="file">Plik CSV:</label>
         </div>
-        <Input type="file" id="file" />
+        <CSVReader
+          onUploadAccepted={(results: any) => {
+            setFile(results.data);
+          }}
+        >
+          {({getRootProps, acceptedFile, ProgressBar}: any) => (
+            <Container>
+              <ButtonFile type="button" {...getRootProps()}>
+                Dodaj plik
+              </ButtonFile>
+              <FileBox>{acceptedFile && acceptedFile.name}</FileBox>
+              <ProgressBar className="progress-bar" />
+            </Container>
+          )}
+        </CSVReader>
       </InputWrapper>
       <div className="btn-box">
         <Button text="Zapisz" type="submit" />
@@ -18,3 +43,23 @@ export const AddStudentsForm = () => {
     </FormAccount>
   );
 };
+
+const ButtonFile = styled.button`
+  background-color: ${(props) => props.theme.colors.darkBlue};
+  color: ${(props) => props.theme.colors.white};
+  border: none;
+  cursor: pointer;
+  padding: ${(props) => props.theme.paddingSize.sm};
+  white-space: nowrap;
+`;
+
+const FileBox = styled.div`
+  color: ${(props) => props.theme.colors.white};
+  margin: 0 ${(props) => props.theme.marginSize.base};
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 55%;
+`;
