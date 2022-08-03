51356d6dbe83d3451a549d35ad4470b770984e99
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-08-03 10:58:56 +0200

    Added adminSlice

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {authSlice} from './features/authSlice';
+import {adminSlice} from './features/adminSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    admin: adminSlice.reducer,
   },
 });
 
